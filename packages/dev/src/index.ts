389e4f03365879dc556913fcc4b7882620ae8d22
Artur <busanix@gmail.com>
2023-09-01 15:58:39 -0300

    dev

--- packages/dev/src/index.ts
+++ packages/dev/src/index.ts
@@ -2,6 +2,8 @@ import { Interpreter } from 'interpreter'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
+import { readFile } from 'fs/promises'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
@@ -9,7 +11,8 @@ const interpreter = new Interpreter()
 
 const start = async () => {
   const filePath = join(__dirname, 'script.vi')
-  const result = await interpreter.executeFile(filePath)
+  const file = await readFile(filePath, 'utf-8')
+  const result = await interpreter.execute(file)
 }
 
 start()
